d8d55d411dd372ad420690ab801df72e8cfb7f91
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-03 14:58:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "22182189"))
+API_HASH = getenv("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6556141430").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+LOG_GROUP = getenv("LOG_GROUP", "-1002227803088")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002227803088"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78c")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", None)
 INSTA_COOKIES = getenv("INSTA_COOKIES", None)
