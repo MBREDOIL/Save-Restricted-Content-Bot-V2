132f6edc0a4aba1b9fed09e68e8a676eeb7a49a0
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-03 16:52:21 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -40,12 +40,12 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
-🗓️ Created: 2025-01-11
-🔄 Last Modified: 2025-01-11
+👨‍💻 Author: UIO
+🌐 GitHub: https://github.com
+📬 Telegram: https://t.me/UIHASH
+▶️ YouTube: https://youtube.com
+🗓️ Created: 2025-02-03
+🔄 Last Modified: 2025-02-03
 🛠️ Version: 2.0.5
 📜 License: MIT License
 ---------------------------------------------------
