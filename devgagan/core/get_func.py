a1019c187f9dca4f2d4cbbd02eb65fd3551af6a1
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-05 19:24:55 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -229,7 +229,7 @@ async def get_msg(userbot, sender, edit_id, msg_link, i, message):
             return
             
         # Fetch the target message
-        msg = await app.get_messages(chat, msg_id)
+        msg = await userbot.get_messages(chat, msg_id)
         if not msg or msg.service or msg.empty:
             return
 
@@ -422,7 +422,7 @@ async def copy_message_with_chat_id(app, userbot, sender, chat_id, message_id, e
         if result is None:
             await edit.edit("Trying if it is a group...")
             chat_id = (await userbot.get_chat(f"@{chat_id}")).id
-            msg = await app.get_messages(chat_id, message_id)
+            msg = await userbot.get_messages(chat_id, message_id)
 
             if not msg or msg.service or msg.empty:
                 return
