350313ff3012ec9afd9afc831fbf70fb0f5fad94
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-06 23:15:11 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -14,7 +14,7 @@
 # ---------------------------------------------------
 
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import random
 import requests
 import string
@@ -146,25 +146,3 @@ async def smart_handler(client, message):
         await message.reply("Click the button below to verify your free access token: \n\n> What will you get ? \n1. No time bound upto 3 hours \n2. Batch command limit will be FreeLimit + 20 \n3. All functions unlocked", reply_markup=button)
  
 
-
-
-
-
-LOG_GROUP = -1002227803088  
-
-@Client.on_message(filters.text)
-async def log_user(update: Message):
-    user = update.from_user
-    user_name = user.first_name
-    user_id = user.id
-    
-    # Use .mention to get the proper mention format (works even without a username)
-    user_mention = user.mention
-    
-    # Log the user's details to the log channel
-    log_message = (f"User interacted with bot: {user_name} ({user_mention})\n"
-                   f"User ID: {user_id}\n"
-                   f"Message: {update.text}")
-    
-    # Ensure to await the asynchronous operation of sending the log message
-    await Client.send_message(chat_id=LOG_GROUP, text=log_message)
