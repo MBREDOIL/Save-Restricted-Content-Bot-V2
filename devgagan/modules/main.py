7c2895f7da0f7198e08a002c66cce84be4e96c68
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-06 22:43:48 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -26,6 +26,7 @@ from devgagan.core.mongo import db
 from pyrogram.errors import FloodWait
 from datetime import datetime, timedelta
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message
 import subprocess
 from devgagan.modules.shrink import is_user_verified
 async def generate_random_name(length=8):
@@ -285,6 +286,33 @@ async def batch_link(_, message):
         await app.send_message(message.chat.id, f"Error: {e}")
     finally:
         users_loop.pop(user_id, None)
+
+
+
+
+
+
+# Replace with your log channel ID (can be public like @yourlogchannel or private like -1001234567890)
+LOG_GROUP =  '-1002227803088'  # or the channel ID in numeric form (e.g., -1001234567890)
+
+@app.on_message(filters.text)
+async def log_user(update: Message):
+    user = update.from_user
+    user_name = user.first_name
+    user_id = user.id
+    
+    # Use .mention to get the proper mention format (works even without a username)
+    user_mention = user.mention
+    
+    # Log the user's details to the log channel
+    log_message = (f"User interacted with bot: {user_name} ({user_mention})\n"
+                   f"User ID: {user_id}\n"
+                   f"Message: {update.text}")
+    
+    # Ensure to await the asynchronous operation of sending the log message
+    await app.send_message(chat_id=LOG_GROUP, text=log_message)
+
+
 
 @app.on_message(filters.command("cancel"))
 async def stop_batch(_, message):
