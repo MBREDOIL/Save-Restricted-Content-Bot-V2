fcdbd831dae80a2da452be7966b606d87939230d
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-02-06 23:38:30 +0530

    Update logs.py

--- devgagan/modules/logs.py
+++ devgagan/modules/logs.py
@@ -1,12 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+LOG_GROUP = '-1002227803088'
 
-# Replace with your log channel ID (can be public like @yourlogchannel or private like -1001234567890)
-LOG_GROUP =  '-1002227803088'  # or the channel ID in numeric form (e.g., -1001234567890)
-
-@app.on_message(filters.text)
-async def log_user(client, message):
+@Client.on_message(filters.text)
+async def log_user(update: Message):
     user = update.from_user
     user_name = user.first_name
     user_id = user.id
@@ -20,4 +18,5 @@ async def log_user(client, message):
                    f"Message: {update.text}")
     
     # Ensure to await the asynchronous operation of sending the log message
-    await app.send_message(chat_id=LOG_GROUP, text=log_message)
+    await Client.send_message(chat_id=LOG_GROUP, text=log_message)
+    
